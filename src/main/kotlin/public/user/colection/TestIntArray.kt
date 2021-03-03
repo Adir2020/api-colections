424f13c6ccdf6a5b4a885b212984c9b4ddeaa6d7
Adir2020 <67104058+Adir2020@users.noreply.github.com>
2021-03-03 10:11:15 -0300

    Initial commit

---
+++ src/main/kotlin/public/user/colection/TestIntArray.kt
@@ -0,0 +1,18 @@
+package public.user.colection
+
+fun main() {
+    val values = IntArray(  5 )
+    values [0] = 1
+    values [1] = 2
+    values [2] = 3
+    values [3] = 4
+    values [4] = 5
+
+    for (valor in values) {
+        println(valor)
+    }
+
+    values.forEach { valor ->
+      println(valor)
+    }
+}
