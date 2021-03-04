a6c1b22d3e667825619ccf644b7b4357f60f45dd
Adir2020 <67104058+Adir2020@users.noreply.github.com>
2021-03-04 08:49:02 -0300

    create kotlin listOf

---
+++ src/main/kotlin/public/user/colection/TestList.kt
@@ -0,0 +1,24 @@
+package public.user.colection
+
+fun main() {
+    val joao = officer( name = "joao", salary = 1000.0 )
+    val adir = officer( name = "adir", salary = 9000.0 )
+    val pedro = officer( name = "pedro", salary = 2000.0 )
+
+    val officers = listOf(joao, adir, pedro)
+
+    officers.forEach { println(it) }
+    println("-----------------------------")
+    println(officers.find { it.name == "adir"})
+}
+
+data class officer(
+    val name: String,
+    val salary: Double
+){
+    override fun toString(): String =
+        """
+        name:   $name
+        salary: $salary
+        """.trimIndent()
+    }
