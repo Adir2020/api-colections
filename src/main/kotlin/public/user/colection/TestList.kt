e1a3853a29de0be5a230c943c357043180e5897d
Adir2020 <67104058+Adir2020@users.noreply.github.com>
2021-03-04 09:56:33 -0300

    create kotlin groups or groupBy

--- src/main/kotlin/public/user/colection/TestList.kt
+++ src/main/kotlin/public/user/colection/TestList.kt
@@ -1,24 +1,32 @@
 package public.user.colection
 
 fun main() {
-    val joao = officer( name = "joao", salary = 1000.0 )
-    val adir = officer( name = "adir", salary = 9000.0 )
-    val pedro = officer( name = "pedro", salary = 5000.0 )
+    val joao = officer( name = "joao", salary = 1000.0, typeContract = "CLT"  )
+    val adir = officer( name = "adir", salary = 9000.0, typeContract = "PJ" )
+    val pedro = officer( name = "pedro", salary = 5000.0, typeContract = "CLT")
 
     val officers = listOf(joao, adir, pedro)
 
     officers.forEach { println(it) }
     println("-----------------------------")
+
     println(officers.find { it.name == "adir"})
 
     println("-----------------------------")
 
     officers.sortedBy { it.salary }.forEach { println(it) }
+
+    println("-----------------------------")
+
+    officers
+        .groupBy { it.typeContract }
+        .forEach { println(it) }
 }
 
 data class officer(
     val name: String,
-    val salary: Double
+    val salary: Double,
+    val typeContract: String
 ){
     override fun toString(): String =
         """
