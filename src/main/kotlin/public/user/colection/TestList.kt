3b5bfe8c1c755ab8aecb6d66400e2366e8bc7fb6
Adir2020 <67104058+Adir2020@users.noreply.github.com>
2021-03-04 08:59:16 -0300

    create kotlin list or sortedBy

--- src/main/kotlin/public/user/colection/TestList.kt
+++ src/main/kotlin/public/user/colection/TestList.kt
@@ -3,13 +3,17 @@ package public.user.colection
 fun main() {
     val joao = officer( name = "joao", salary = 1000.0 )
     val adir = officer( name = "adir", salary = 9000.0 )
-    val pedro = officer( name = "pedro", salary = 2000.0 )
+    val pedro = officer( name = "pedro", salary = 5000.0 )
 
     val officers = listOf(joao, adir, pedro)
 
     officers.forEach { println(it) }
     println("-----------------------------")
     println(officers.find { it.name == "adir"})
+
+    println("-----------------------------")
+
+    officers.sortedBy { it.salary }.forEach { println(it) }
 }
 
 data class officer(
