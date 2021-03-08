ae4258b7f0f34c55f9ca846b97634f61ff339d90
Adir2020 <67104058+Adir2020@users.noreply.github.com>
2021-03-08 10:50:35 -0300

    create kotlin mutableMapOf

---
+++ src/main/kotlin/public/user/colection/TestMutableMap.kt
@@ -0,0 +1,15 @@
+package public.user.colection
+
+fun main() {
+    val joao = officer(name = "joao", salary = 1000.0, typeContract = "CLT")
+    val adir = officer(name = "adir", salary = 9000.0, typeContract = "PJ")
+    val pedro = officer(name = "pedro", salary = 5000.0, typeContract = "CLT")
+
+    val repositorio = Repositorio<officer>()
+
+    repositorio.create(joao.name, joao)
+    repositorio.create(adir.name, adir)
+    repositorio.create(pedro.name, pedro)
+
+    println(repositorio.findById(joao.name))
+}
